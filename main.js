a26106b9f52e71e27aad04a08509ec8609030bfb
Parker Rebensdorf <parker.rebensdorf@gmail.com>
2018-02-21 10:41:23 -0800

    fixed typos in constructor, click handler now changes colors and locations properly

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(initializeApp);
 
-var playerTurn = 1;
+var playerTurn = 2;
 
 function initializeApp(){
     clickHandler();
     var player1 = new Player('John');
-    var player2 = new player1('Susie');
+    var player2 = new Player('Susie');
 }
 
 function clickHandler(){
@@ -13,21 +13,33 @@ function clickHandler(){
 }
 
 function getLocation(){
+    console.log('i have been clicked');
     if(playerTurn === 1){
-        $(this).attr('background-color', 'green')
+        $(this).css('background-color', 'green')
     } else{
-        $(this).attr('background-color', 'yellow')
+        $(this).css('background-color', 'yellow')
     }
     var classes = $(this).attr('class');
     var column = classes.charAt(6);
     var row = classes.charAt(11)
     var currentPlayer = playerTurn;
-    updateBoardArray(row, column, currentPlayer);
+    console.log('column: ' + column);
+    console.log('row: ' + row);
+    console.log('current player: ' + currentPlayer);
+    // updateBoardArray(row, column, currentPlayer);
 }
 
 class Player{
     constructor(name){
         this.name = name || 'Player 1'
     }
-    
+
+}
+
+function changePlayer(){
+    if(playerTurn === 1){
+        playerTurn = 2
+    } else{
+        playerTurn = 1
+    }
 }
